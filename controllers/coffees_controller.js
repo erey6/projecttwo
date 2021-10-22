ef51488d80752ec7a1267051c41590b9ff8982b2
Eric Reyes <ericreyes@gmail.com>
2021-10-21 19:39:34 -0500

    part way through edit form

--- controllers/coffees_controller.js
+++ controllers/coffees_controller.js
@@ -13,23 +13,56 @@ coffees.get('/home', (req, res) => {
             coffees: allCoffees
         })
     })
-    
+
 })
+//INDEX
+coffees.get('/', (req, res) => {
+    Coffee.find({}, (err, allCoffees) => {
+        res.render('coffees/index.ejs', {
+            coffees: allCoffees
+        })
+    })
+
+})
+
+//SHOW
+coffees.get('/:id', (req, res) => {
+    Coffee.find({}, (err1, allCoffees) => {
+        Coffee.findById(req.params.id, (err, foundCoffee) => {
+            (foundCoffee.wholeBean === true) ? foundCoffee.bean = 'Whole bean' : foundCoffee.bean = 'Ground';
+            (foundCoffee.home === true) ? foundCoffee.where = 'home' : foundCoffee.where = 'cafe';
+            res.render('coffees/show.ejs', {
+                coffee: foundCoffee,
+                coffees: allCoffees
+            })
+        })
+    })
+})
+
+//EDIT
+coffees.get('/:id/edit', (req, res) => {
+    Coffee.findById(req.params.id, (err, foundCoffee) => {
+        res.render('coffees/edit.ejs', {
+            coffee: foundCoffee
+        })
+    })
+})
+
 
 
 
 
 
 // Seed route
-coffees.get('/seed', (req, res) => {
-    Coffee.create(coffeeSeed, (error, seed) => {
-        if (error) {
-            console.log(error)
-        } else {
-            res.send('<a href="/coffees/home">seeded </a>')
-        }
-    })
-})
+// coffees.get('/seed', (req, res) => {
+//     Coffee.create(coffeeSeed, (error, seed) => {
+//         if (error) {
+//             console.log(error)
+//         } else {
+//             res.send('<a href="/coffees/home">seeded </a>')
+//         }
+//     })
+// })
 
 
 
