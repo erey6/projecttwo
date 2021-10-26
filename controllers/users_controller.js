1e3c7c3c0546f1ae3cf607e3aa90ea5831a1cf3d
Eric Reyes <ericreyes@gmail.com>
2021-10-25 21:46:10 -0500

    set up new user form

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -4,9 +4,15 @@ const users = express.Router()
 const User = require('../models/users.js')
 
 users.get('/new', (req, res) => {
-    res.send('time to set up a users')
-    // res.render('users/new.ejs')
+    res.render('users/new.ejs')
 })
 
+users.post('/', (req, res) => {
+    req.body.password = bcrypt.hashSync(req.body.password, bcrypt,genSaltSync(10))
+    User.create(req.body, (err, createdUswer) => {
+        console.log('created', createdUser)
+        res.redirect('/')
+    })
+})
 
 module.exports = users
